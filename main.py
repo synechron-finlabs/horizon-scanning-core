ea52c4ed604c7d86352f085b6c52115a1575940a
kumar.sunil <kumar.sunil@synechron.com>
2022-10-27 16:37:45 +0530

    updating code and fixing bugs

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import argparse
 import importlib
 import os
 import json
-
+import services.log as log
+custom_logger = log.get_logger(__name__)
 
 def start():
     """
@@ -11,20 +12,20 @@ def start():
     """
 
     ap = argparse.ArgumentParser()
-    ap.add_argument("-a", "--agency", type=str, default="")
+    ap.add_argument("-a", "--agency", type=str, default="all")
     ap.add_argument("-o", "--output", type=str, default="agnecy.json")
     ap.add_argument("-m", "--month", type=str, default=1)
     args = ap.parse_args()
 
     #Checking for agnecy is passing or not
     if args.agency:
-        agency=args.agency
+        agency=str(args.agency).strip().lower()
         no_month=1
-
+        
         try :
             agency_list=[]
             #If all agency is passing by user
-            if agency.lower()=="all":
+            if agency=="all":
                 dir_list = os.listdir('agencies')
                 agency_list.extend(dir_list)
 
@@ -44,7 +45,7 @@ def start():
                         assert("Please provide month in integer")
             
             for item in agency_list:
-                item=item.lower()
+                
                 module_name = "agencies.{}.{}".format(item,item)
                 module = importlib.import_module(module_name)
                 obj_module = getattr(module, 'scanning_engine')()
@@ -55,12 +56,13 @@ def start():
                 out_file_name = out_file_name + ".json"
                 with open(out_file_name, 'w') as f:
                     f.write(json.dumps(results))
-
-            #print(results)
+            
         except Exception as ex:
-            print(ex)
+            custom_logger.error("Error while scanning {} .".format(ex))
     else:
+        custom_logger.info("Please provide agency name")
         assert("Please provide agency name")
+        
         
 if __name__ == "__main__":
     start()
